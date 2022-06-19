be0dff22ccfc2ac1ec0620d98f5455a200592cb8
Sarah Gebauer <xgebi@users.noreply.github.com>
2022-06-19 11:42:30 +0200

    clean up 🎉

--- app/services/process_feed_service.rb
+++ app/services/process_feed_service.rb
@@ -13,13 +13,14 @@ class ProcessFeedService
       rss_doc.css('item').map do |item|
         next if ArticleContent.find_by(guid: item.at_css('guid').content)
 
-        ac = ArticleContent.new
-        ac.guid = item.at_css('guid').content
-        ac.title = item.at_css('title').content
-        ac.description = item.at_css('description').content
-        ac.content = item.at_css('content|encoded').content
-        ac.pub_date = item.at_css('pubDate').content
-        ac.link = item.at_css('link').content
+        ac = ArticleContent.new(
+          guid: item.at_css('guid').content,
+          title: item.at_css('title').content,
+          description: item.at_css('description').content,
+          content: item.at_css('content|encoded').content,
+          pub_date: item.at_css('pubDate').content,
+          link: item.at_css('link').content
+        )
         ac.save!
       end
     end
@@ -35,16 +36,17 @@ class ProcessFeedService
       rss_doc.css('item').map do |item|
         next if ArticleContent.find_by(guid: item.at_css('guid').content)
 
-        ac = ArticleContent.new
-        ac.guid = item.at_css('guid').content
-        ac.title = item.at_css('title').content
-        ac.media_link = item.at_css('enclosure').content
-        ac.itunes_duration = item.at_css('itunes|duration').content
-        ac.itunes_summary = item.at_css('itunes|summary').content
-        ac.description = item.at_css('description').content
-        ac.content = item.at_css('content|encoded').content
-        ac.pub_date = item.at_css('pubDate').content
-        ac.link = item.at_css('link').content
+        ac = ArticleContent.new(
+          guid: item.at_css('guid').content,
+          title: item.at_css('title').content,
+          description: item.at_css('description').content,
+          media_link: item.at_css('enclosure').content,
+          itunes_duration: item.at_css('itunes|duration').content,
+          itunes_summary: item.at_css('itunes|summary').content,
+          content: item.at_css('content|encoded').content,
+          pub_date: item.at_css('pubDate').content,
+          link: item.at_css('link').content
+        )
         ac.save!
       end
     end
